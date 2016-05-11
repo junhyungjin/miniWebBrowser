85d786085cf489a90dd64712f4bfdbf2281ffd2a
Hyung jin Jun <junhyungjin@me.com>
2016-05-12 01:20:01 +0900

    mini web browser

--- miniWebBrowser/ViewController.swift
+++ miniWebBrowser/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIWebViewDelegate {
 
+    @IBOutlet var bookMarkSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet var urlTextField: UITextField!
+    @IBOutlet var mainWebView: UIWebView!
+    @IBOutlet var spinningActivityIndicatorView: UIActivityIndicatorView!
+    
+    @IBAction func bookMarkAction(sender: AnyObject) {
+        
+        let bookMarkUrl = bookMarkSegmentedControl.titleForSegmentAtIndex(bookMarkSegmentedControl.selectedSegmentIndex)
+        let urlString = "http://www.\(bookMarkUrl!).com"
+        mainWebView.loadRequest(NSURLRequest(URL: NSURL(string: urlString)!))
+        urlTextField.text = urlString.lowercaseString
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        var urlString = "\(urlTextField.text!)"
+        if !urlString.hasPrefix("http://") {
+            urlString = "http://\(urlTextField.text!)"
+        }
+        
+        mainWebView.loadRequest(NSURLRequest(URL: NSURL(string: urlString)!))
+        textField.resignFirstResponder()
+        return true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let urlString = "http://www.facebook.com"
+        mainWebView.loadRequest(NSURLRequest(URL: NSURL(string: urlString)!))
+        urlTextField.text = urlString.lowercaseString
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        spinningActivityIndicatorView.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        spinningActivityIndicatorView.stopAnimating()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
